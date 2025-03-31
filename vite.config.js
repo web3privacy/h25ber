f271cb2b45e1ec9f8b43e17adc872ee3b2e23400
Mf <38403795+DeBelg@users.noreply.github.com>
2025-03-31 17:40:37 +0200

    update index.html

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: './',
+  srcDir: 'src',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        info: resolve(__dirname, 'info.html'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@styles': resolve(__dirname, './src'),
+      '@scripts': resolve(__dirname, './src')
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+}) 
